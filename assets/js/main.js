fcca5e0b9e405b06bb32e883530e0ec5bbd74a4d
Emanuel Nzinga Maimona <emanuelmaimona@gmail.com>
2022-08-27 00:04:39 +0200

    add special hover on links

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,6 +19,28 @@ const navLink = document.querySelectorAll(".nav-link")
 function linkAction() {
     const navMenu = document.getElementById("main-nav")
     navMenu.classList.remove("show-menu")
+   
+
 }
 
 navLink.forEach((link)=> link.addEventListener('click', linkAction))
+
+
+// ACTIVE SECTION LINK ===================>
+const sections = document.querySelectorAll("section[id")
+function ScrollActive() {
+    const scrollY = window.pageYOffset;
+    sections.forEach((section)=> {
+         const sectionHeight = section.offsetHeight;
+         const sectionTop =  section.offsetTop - 50;
+         sectionId = section.getAttribute('id')
+
+         if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.main-nav a[href*=' + sectionId + ']').classList.add('active-link')
+         } else {
+            document.querySelector('.main-nav a[href*=' + sectionId + ']').classList.remove('active-link')
+         }
+    })
+}
+
+window.addEventListener('scroll', ScrollActive)
