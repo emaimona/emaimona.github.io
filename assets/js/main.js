64bc940be074bd975f5eed2b168abd9155d3eece
Emanuel Nzinga Maimona <emanuelmaimona@gmail.com>
2022-08-26 23:35:50 +0200

    remove menu on click for mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,3 +11,14 @@ navOpen.addEventListener('click',  ()=>{
 navClose.addEventListener('click', ()=>{
     navMenu.classList.remove("show-menu")
 })
+
+
+// REMOVE MENU ON CLICK =================>
+const navLink = document.querySelectorAll(".nav-link")
+
+function linkAction() {
+    const navMenu = document.getElementById("main-nav")
+    navMenu.classList.remove("show-menu")
+}
+
+navLink.forEach((link)=> link.addEventListener('click', linkAction))
